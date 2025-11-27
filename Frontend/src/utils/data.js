dfec7718b5412d2844174f1c96bd35c510f4304a
Milan Gauswami <gauswamimilan17@gmail.com>
2025-11-27 17:07:44 +0530

    changes

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,25 @@
-export const USER_API_ENDPOINT = "https://jobmitr.onrender.com/api/user";
-export const JOB_API_ENDPOINT = "https://jobmitr.onrender.com/api/job/get";
-export const APPLICATION_API_ENDPOINT = "https://jobmitr.onrender.com/api/application";
-export const COMPANY_API_ENDPOINT = "https://jobmitr.onrender.com/api/company";
+// 
+
+const BASE_URL = "https://jobmitr.onrender.com";
+
+export const USER_LOGIN_API = `${BASE_URL}/api/user/login`;
+export const USER_REGISTER_API = `${BASE_URL}/api/user/register`;
+export const USER_PROFILE_API = `${BASE_URL}/api/user/profile`;
+
+// JOB ROUTES
+export const JOB_GET_ALL_API = `${BASE_URL}/api/job/get`;
+export const JOB_GET_SINGLE_API = (id) => `${BASE_URL}/api/job/get/${id}`;
+export const JOB_POST_API = `${BASE_URL}/api/job/post`;
+export const JOB_UPDATE_API = (id) => `${BASE_URL}/api/job/update/${id}`;
+export const JOB_ADMIN_API = `${BASE_URL}/api/job/getadminjobs`;
+
+// COMPANY ROUTES
+export const COMPANY_GET_API = `${BASE_URL}/api/company/get`;
+export const COMPANY_CREATE_API = `${BASE_URL}/api/company/create`;
+export const COMPANY_SINGLE_API = (id) => `${BASE_URL}/api/company/get/${id}`;
+
+// APPLICATION ROUTES
+export const APPLICATION_POST_API = `${BASE_URL}/api/application/apply`;
+export const APPLICATION_GET_API = `${BASE_URL}/api/application/get`;
+export const APPLICATION_BY_JOB_API = (id) =>
+  `${BASE_URL}/api/application/job/${id}`;
