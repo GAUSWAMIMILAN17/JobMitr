4fe61b453f5488fda981cfa4a1084219e19d7b3c
Milan Gauswami <gauswamimilan17@gmail.com>
2025-11-27 17:49:33 +0530

    finel

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://jobmitr.onrender.com"],
+  origin: ["https://jobmitr-1.onrender.com"],
   credentials: true,
 };
 
