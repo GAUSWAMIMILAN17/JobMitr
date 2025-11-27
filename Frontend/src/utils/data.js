2fc64c040bcbe4c47a8164aa65681827b78f73f6
Milan Gauswami <gauswamimilan17@gmail.com>
2025-11-27 17:03:39 +0530

    change

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,4 @@
 export const USER_API_ENDPOINT = "https://jobmitr.onrender.com/api/user";
-export const JOB_API_ENDPOINT = "https://jobmitr.onrender.com/api/job";
+export const JOB_API_ENDPOINT = "https://jobmitr.onrender.com/api/job/get";
 export const APPLICATION_API_ENDPOINT = "https://jobmitr.onrender.com/api/application";
 export const COMPANY_API_ENDPOINT = "https://jobmitr.onrender.com/api/company";
