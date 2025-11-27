580d000dbd20e76a99840e055ddd054841d6d054
Milan Gauswami <gauswamimilan17@gmail.com>
2025-11-27 16:57:03 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://job-mitra-omega.vercel.app"],
+  origin: ["https://jobmitr.vercel.app/"],
   credentials: true,
 };
 
