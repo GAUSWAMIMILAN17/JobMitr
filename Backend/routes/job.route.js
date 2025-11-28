f1bd6edfab8afa5d7bc398ca3c769e6068a4da24
Milan Gauswami <gauswamimilan17@gmail.com>
2025-11-28 16:08:46 +0530

    new react

--- Backend/routes/job.route.js
+++ Backend/routes/job.route.js
@@ -14,6 +14,6 @@ const router = express.Router();
 router.route("/post").post(authenticateToken, postJob);
 router.route("/get").get( getAllJobs);
 router.route("/getadminjobs").get(authenticateToken, getAdminJobs);
-router.route("/get/:id").get(authenticateToken, getJobById);
+router.route("/get/:id").get(getJobById);
 router.route("/update/:id").put(authenticateToken, updateJobs);
 export default router;
