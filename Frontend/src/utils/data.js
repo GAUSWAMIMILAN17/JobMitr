908a1d493800f136abcff84b12192354b9933eb4
Milan Gauswami <your-github-email@example.com>
2026-06-16 11:43:23 +0530

    Api Done

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,12 +1,13 @@
-export const USER_API_ENDPOINT = "http://localhost:5001/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:5001/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:5001/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:5001/api/company";
-export const CHAT_API_ENDPOINT = "http://localhost:5001/api/chat";
+// export const USER_API_ENDPOINT = "http://localhost:5001/api/user";
+// export const JOB_API_ENDPOINT = "http://localhost:5001/api/job";
+// export const APPLICATION_API_ENDPOINT = "http://localhost:5001/api/application";
+// export const COMPANY_API_ENDPOINT = "http://localhost:5001/api/company";
+// export const CHAT_API_ENDPOINT = "http://localhost:5001/api/chat";
 
-// export const USER_API_ENDPOINT = "https://jobmitr.onrender.com/api/user";
-// export const JOB_API_ENDPOINT = "https://jobmitr.onrender.com/api/job";
-// export const APPLICATION_API_ENDPOINT = "https://jobmitr.onrender.com/api/application";
-// export const COMPANY_API_ENDPOINT = "https://jobmitr.onrender.com/api/company";
+export const USER_API_ENDPOINT = "https://jobmitr.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://jobmitr.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://jobmitr.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://jobmitr.onrender.com/api/company";
+export const CHAT_API_ENDPOINT = "https://jobmitr.onrender.com/api/chat";
 
 
